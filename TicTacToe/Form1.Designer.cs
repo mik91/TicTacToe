968f5c040784272951c75d521a06ef674c445eec
Mikhail Kamara <kamaramikhail@gmail.com>
2021-05-25 15:04:31 -0400

    Verification of columns,diagonals and rows for TIc Tac toe

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tblPanel = new System.Windows.Forms.TableLayoutPanel();
             this.btn9 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
             this.btn7 = new System.Windows.Forms.Button();
@@ -42,7 +42,7 @@
             this.btn2 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tblPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -62,40 +62,41 @@
             this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
             this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 19);
             this.newGameToolStripMenuItem.Text = "New Game";
-            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.NewGameToolStripMenuItem_Click);
             // 
             // infoToolStripMenuItem
             // 
             this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
             this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 19);
             this.infoToolStripMenuItem.Text = "Info";
-            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.InfoToolStripMenuItem_Click);
             // 
-            // tableLayoutPanel1
+            // tblPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1368, 1244);
-            this.tableLayoutPanel1.TabIndex = 1;
+            this.tblPanel.AutoScroll = true;
+            this.tblPanel.ColumnCount = 3;
+            this.tblPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.Controls.Add(this.btn9, 2, 2);
+            this.tblPanel.Controls.Add(this.btn8, 1, 2);
+            this.tblPanel.Controls.Add(this.btn7, 0, 2);
+            this.tblPanel.Controls.Add(this.btn6, 2, 1);
+            this.tblPanel.Controls.Add(this.btn5, 1, 1);
+            this.tblPanel.Controls.Add(this.btn4, 0, 1);
+            this.tblPanel.Controls.Add(this.btn3, 2, 0);
+            this.tblPanel.Controls.Add(this.btn2, 1, 0);
+            this.tblPanel.Controls.Add(this.btn1, 0, 0);
+            this.tblPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblPanel.Location = new System.Drawing.Point(0, 27);
+            this.tblPanel.Margin = new System.Windows.Forms.Padding(6);
+            this.tblPanel.Name = "tblPanel";
+            this.tblPanel.RowCount = 3;
+            this.tblPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tblPanel.Size = new System.Drawing.Size(1368, 1244);
+            this.tblPanel.TabIndex = 1;
             // 
             // btn9
             // 
@@ -228,7 +229,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1368, 1271);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tblPanel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -240,7 +241,7 @@
             this.Text = "TicTacToe";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tblPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,7 +252,7 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tblPanel;
         private System.Windows.Forms.Button btn9;
         private System.Windows.Forms.Button btn8;
         private System.Windows.Forms.Button btn7;
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,6 +12,11 @@ namespace TicTacToe
 {
     public partial class TicTacToe : Form
     {
+        bool isXTurn = false;
+        static int size = 3;
+        short movement = 0;
+        int[,] array = new int[size, size];
+
         public TicTacToe()
         {
             InitializeComponent();
@@ -19,17 +24,61 @@ namespace TicTacToe
 
         private void btn_Click(object sender, EventArgs e)
         {
+            Button bt = sender as Button;
+            bt.Enabled = false;
+            bt.BackColor = Color.Olive;
+            TableLayoutPanelCellPosition pos = tblPanel.GetPositionFromControl(bt);
+            if (isXTurn) {
+                bt.Text = "X";
+                array[pos.Row, pos.Column] = 1;
+            }
+            else {
+                bt.Text = "O";
+                array[pos.Row, pos.Column] = 2;
+            }
 
+            bool gamefinished = GameFinished(pos, isXTurn == true?1:2);
+            isXTurn = !isXTurn;
         }
 
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        private void InfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private Boolean GameFinished(TableLayoutPanelCellPosition pos, int type)
+        {
+            bool column = false;
+            bool row = false;
+            bool diagonals = false; 
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (array[j,i] != type)
+                        column = false;
+                    if (array[i, j] != type)
+                        row = false;
+                    if (i == j && array[i, j] == type)
+                        diagonals = false;
+                }
+            }
+
+            return diagonals || column || row;
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        if (array[i, j] != type)
+            //            break;
+            //    }
+            //}
         }
     }
 }
